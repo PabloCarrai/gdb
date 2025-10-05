bbb991a3173c960625209466f02336a1fb476646
PabloCarrai <pablohernanjuan@hotmail.com>
2025-10-05 19:44:00 -0300

    Arrancamos

---
+++ gdb.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+
+
+class ventanas:
+    def __init__(self):
+        self.ventana = Tk()
+        self.ventana.title("Bienvenido a gdb")
+        self.ventana.mainloop()
+
+
+prueba = ventanas()
